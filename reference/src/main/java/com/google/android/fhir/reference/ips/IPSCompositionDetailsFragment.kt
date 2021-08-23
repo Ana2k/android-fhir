1d75361d4d51b297311985b0643b9da6dbc86f60
Ana2k <anushkachakra@gmail.com>
2021-08-23 21:10:55 +0530

    dummy data showing

--- reference/src/main/java/com/google/android/fhir/reference/ips/IPSCompositionDetailsFragment.kt
+++ reference/src/main/java/com/google/android/fhir/reference/ips/IPSCompositionDetailsFragment.kt
@@ -1,15 +1,20 @@
 package com.google.android.fhir.reference.ips
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.fhir.reference.PatientDetailsFragmentArgs
 import com.google.android.fhir.reference.databinding.IpsCompositionDetailViewBinding
 
 class IPSCompositionDetailsFragment: Fragment() {
     private var _binding: IpsCompositionDetailViewBinding? = null
     private val binding get() = _binding!!
+    private val args: PatientDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,4 +27,14 @@ class IPSCompositionDetailsFragment: Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //extract the argument files.
+        //make a textview. text
+        //then show that in those
+        //from android docs:-
+        val textView = binding.detailsTextView
+        val arguments = IPSCompositionDetailsFragmentArgs.fromBundle(requireArguments())
+        Log.d("InsideDetsComposition",arguments.ipsPatientDetail.toString())
+    }
 }
--- reference/src/main/java/com/google/android/fhir/reference/ips/IPSCompositionListFragment.kt
+++ reference/src/main/java/com/google/android/fhir/reference/ips/IPSCompositionListFragment.kt
@@ -2,7 +2,9 @@ package com.google.android.fhir.reference.ips
 
 import android.os.Bundle
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -10,8 +12,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.fhir.reference.PatientListFragmentDirections
-import com.google.android.fhir.reference.PatientListViewModel
 import com.google.android.fhir.reference.databinding.IpsCompositionListViewBinding
 import org.hl7.fhir.r4.model.Composition
 import kotlin.collections.ArrayList
